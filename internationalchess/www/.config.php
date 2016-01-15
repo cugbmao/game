961202d257807ac74eac132bc647e5d12290e99a
maowei <maowei@metrodata.cn>
2016-01-15 15:39:50 +0800

    update:

---
+++ internationalchess/www/.config.php
@@ -0,0 +1,6 @@
+<?php
+define('APP_PATH', '/Users/mao/git/chessboardjs/');
+define('BASE_URL', 'http://localhost/chess/');
+define('IN_PRODUCTION', true);
+define('PROJECT_NAME', 'chessboard.js');
+?>
