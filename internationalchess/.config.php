c9816b2dc247c189bd7392d6e15273e33660a942
maowei <maowei@metrodata.cn>
2016-01-15 16:47:21 +0800

    update

---
+++ internationalchess/.config.php
@@ -0,0 +1,6 @@
+<?php
+define('APP_PATH', '/alidata/www/game/internationalchess/');
+define('BASE_URL', 'http://game.cugbmao.com/internationalchess/');
+define('IN_PRODUCTION', true);
+define('PROJECT_NAME', 'chessboard.js');
+?>
